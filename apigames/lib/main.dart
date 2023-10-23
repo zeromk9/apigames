bc0fbc932abf05ea6136a594fb62efcc31c5bf54
zeromk9 <zeromarthk@hotmail.com>
2023-10-22 20:17:24 -0600

    c3:main

--- apigames/lib/main.dart
+++ apigames/lib/main.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:apigames/providers/providers.dart';
+import 'package:apigames/screens/screens.dart';
 
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => GamesProvider(),
+          lazy: false,
+        )
+      ],
+      child: const MyApp(),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MONSTER HUNTER',
+      initialRoute: '/home',
+      routes: {
+        '/home': (_) => const HomeScreen(),
+        '/details': (_) => const DetailsScreen()
+      },
+    );
+  }
+}
